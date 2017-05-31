13e745cd434e82cb5f6370dc39d964bc50b343de
Adam Mortka <a.mortka@gmail.com>
2017-05-31 14:14:10 +0200

    initial commit from vue webpack template

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  globals: {
+    'google': true
+  },
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-use-before-define': ['error', { 'functions': false, 'classes': true }],
+    'max-len': ['error', 120, 4, { 'ignoreComments': true }],
+    'space-before-function-paren': ['error', 'never'],
+    'linebreak-style': ['error', 'windows'],
+    'no-bitwise': ['error', { 'allow': ['~'] }],
+  },
+};
---
+++ src/components/Scene.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Scene">
+    <p>hello world!</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'scene',
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
